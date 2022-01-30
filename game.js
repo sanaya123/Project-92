9fb764b72886b1015e2fdb8fa686edd0f0a41a10
sanaya123 <83179671+sanaya123@users.noreply.github.com>
2022-01-30 09:41:16 -0600

    Add files via upload

--- game.js
+++ game.js
@@ -34,20 +34,20 @@ question_turn="player1";
 answer_turn="player2";
 
 function check(){
-get_answer = document.getElementById("input_text_box").value;
+get_answer = document.getElementById("input_check_box").value;
 
 if (get_answer==actual_answer)
 {
 
     if(answer_turn=="player1"){
 
-        update_player1_score = player1_score + 1;
-        document.getElementById("player1_score").innerHTML = update_player1_score;
+        player1_score = player1_score + 1;
+        document.getElementById("player1_score").innerHTML = player1_score;
     }
     else
     {
-        update_player2_score = player2_score + 1;
-        document.getElementById("player2_score").innerHTML = update_player2_score;
+        player2_score = player2_score + 1;
+        document.getElementById("player2_score").innerHTML = player2_score;
     }
 
 
@@ -63,6 +63,19 @@ else
     question_turn = "player1"
     document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name ;
 }
+
+if(answer_turn == "player1")
+{
+    answer_turn = "player2"
+    document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name ;
+}
+else 
+{
+    answer_turn = "player1"
+    document.getElementById("player_answer").innerHTML = "Answer Turn - " + player1_name ;
+}
+
+
 
 
 }
